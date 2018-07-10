c36d144b14d6abb303e1b5a5ca4e457bd3143e14
Keshab Budhathoky <Keshab.Budhathoky@riverbed.com>
2018-07-09 22:02:41 -0500

    Print data in PrettyTable format

--- stocks.py
+++ stocks.py
@@ -92,12 +92,22 @@ def get_stock_info(ticker, name=None, days=None):
     except requests.HTTPError as e:
         raise StockHTTPError(e)
     data = resp.json()
+    symbol_table = PrettyTable(['Name', 'Ticker', 'Sector', 'Industry', 'Date', 'Company Founded', 'CEO', 'IPO Date', 'Total Employee', 'Market Cap'])
     if name is not None:
-        click.echo("Name: {}".format(name))
-    click.echo("Ticker: {}, Sector: {}, Industry:{}, Date: {}".\
-               format(click.style(data['Meta Data']['2. Symbol'], fg='red'),
-                      yahoo_ticker_obj.sector, yahoo_ticker_obj.industry,
-                      today))
+        symbol_table.add_row([name, data['Meta Data']['2. Symbol'], yahoo_ticker_obj.sector, yahoo_ticker_obj.industry, today,
+                              rb_client.company_founded, rb_client.company_ceo, rb_client.company_ipo_date,
+                              rb_client.company_employees_total, market_cap(rb_client.market_cap)])
+        #click.echo("Name: {}".format(name))
+    else:
+        symbol_table.add_row(['None', data['Meta Data']['2. Symbol'], yahoo_ticker_obj.sector, yahoo_ticker_obj.industry, today,
+                              rb_client.company_founded, rb_client.company_ceo, rb_client.company_ipo_date,
+                              rb_client.company_employees_total, market_cap(rb_client.market_cap)])
+        #symbol_table.add_row(['None', data['Meta Data']['2. Symbol'], yahoo_ticker_obj.sector, yahoo_ticker_obj.industry, today])
+    #click.echo("Ticker: {}, Sector: {}, Industry:{}, Date: {}".\
+    #           format(click.style(data['Meta Data']['2. Symbol'], fg='red'),
+    #                  yahoo_ticker_obj.sector, yahoo_ticker_obj.industry,
+    #                  today))
+    click.echo(symbol_table)
     click.echo("Company Summary: {}".format(rb_client.company_info))
     price_data = data['Time Series (Daily)']
     close_price_list = []
@@ -117,52 +127,75 @@ def get_stock_info(ticker, name=None, days=None):
         prev_date = datetime.strftime(prev_date, '%Y-%m-%d')
         if k < end_date:
             click.echo(ticker_table)
-            click.echo("Company Founded: {}\t\tCEO: {}\tIPO date: {}\t\t"
-                       "Total Employee: {}".format(click.style(str(rb_client.company_founded), fg='red'),
-                        rb_client.company_ceo, click.style(rb_client.company_ipo_date, fg='red'),
-                        rb_client.company_employees_total))
-            click.echo("Market Cap: {}\tPE: {}\t\tEPS: {}\t\t\t"
-                       "Dividend Yield: {}\t\tDividend Per Share: {}\t"
-                       "Estimated EPS: {}".\
-                        format(market_cap(rb_client.market_cap),
-                        click.style(str(rb_client.company_pe_ratio), fg='red'),
-                        ticker_obj.eps,
-                        ticker_obj.dividend,
-                        ticker_obj.annualDividend,
-                        ticker_obj.estEarnings))
-            click.echo("Trailing PE: {}\t\tForward PE: {}\tP/S (ttm): {}\t\t\t"
-                       "P/B (mrq): {}\t\t\tBeta: {}\t\t\tProfit Margin: {}\n"
-                       "Operating Margin(ttm): {}\tROE: {}\t\tROA: {}\t\t\tRevenue per Share: {}\t"
-                       "Gross Profit(ttm): {}\tCurrent Ratio (mrq): {}\nQuarterly Revenue Growth (yoy): {}\t\t\t"
-                       "Quarterly Earnings Growth (yoy): {}\t\t\t\tTotal Debt/Equity (mrq): {}\n"
-                       "Book Value Per Share (mrq): {}\t\t\tTotal Cash Per Share (mrq): {}\t\t\t\t 1 yr target: {}".
-                       format(yahoo_equity_info['Trailing P/E'], yahoo_equity_info['Forward P/E 1'],
-                              yahoo_equity_info['Price/Sales (ttm)'], yahoo_equity_info['Price/Book (mrq)'],
-                              yahoo_equity_info['Beta'], yahoo_equity_info['Profit Margin'],
-                              yahoo_equity_info['Operating Margin (ttm)'],
-                              yahoo_equity_info['Return on Equity (ttm)'],
-                              yahoo_equity_info['Return on Assets (ttm)'],
-                              yahoo_equity_info['Revenue Per Share (ttm)'],
-                              yahoo_equity_info['Gross Profit (ttm)'],
-                              yahoo_equity_info['Current Ratio (mrq)'],
-                              yahoo_equity_info['Quarterly Revenue Growth (yoy)'],
-                              yahoo_equity_info['Quarterly Earnings Growth (yoy)'],
+            #company_info_table = PrettyTable(['Company Founded', 'CEO', 'IPO Date', 'Total Employee', 'Market Cap'])
+            #company_info_table.add_row([rb_client.company_founded, rb_client.company_ceo, rb_client.company_ipo_date,
+            #                            rb_client.company_employees_total, market_cap(rb_client.market_cap)])
+            #click.echo(company_info_table)
+            #click.echo("Company Founded: {}\t\tCEO: {}\tIPO date: {}\t\t"
+            #           "Total Employee: {}".format(click.style(str(rb_client.company_founded), fg='red'),
+            #            rb_client.company_ceo, click.style(rb_client.company_ipo_date, fg='red'),
+            #            rb_client.company_employees_total))
+            snap_ratio_table = PrettyTable(['PE', 'EPS', 'Estimated EPS', 'Trailing PE', 'Forward PE',
+                                            'P/S (ttm)', 'P/B (mrq)', 'Current Ratio (mrq)', 'Gross Profit(ttm)', 'Profit Margin', 'Operating Margin(ttm)', 'ROE', 'ROA'])
+            snap_ratio_table.add_row([rb_client.company_pe_ratio, ticker_obj.eps, ticker_obj.estEarnings, yahoo_equity_info['Trailing P/E'],
+                                      yahoo_equity_info['Forward P/E 1'], yahoo_equity_info['Price/Sales (ttm)'], yahoo_equity_info['Price/Book (mrq)'],
+                                      yahoo_equity_info['Current Ratio (mrq)'], yahoo_equity_info['Gross Profit (ttm)'],
+                                      yahoo_equity_info['Profit Margin'], yahoo_equity_info['Operating Margin (ttm)'],
+                                      yahoo_equity_info['Return on Equity (ttm)'], yahoo_equity_info['Return on Assets (ttm)']])
+            click.echo(snap_ratio_table)
+            #click.echo("Market Cap: {}\tPE: {}\t\tEPS: {}\t\t\t"
+            #           "Dividend Yield: {}\t\tDividend Per Share: {}\t"
+            #           "Estimated EPS: {}".\
+            #            format(market_cap(rb_client.market_cap),
+            #            click.style(str(rb_client.company_pe_ratio), fg='red'),
+            #            ticker_obj.eps,
+            #            ticker_obj.dividend,
+            #            ticker_obj.annualDividend,
+            #            ticker_obj.estEarnings))
+            snap_financial_table = PrettyTable(['Revenue per Share', 'Qtr Revenue Growth (yoy)', 'Qtr Earnings Growth (yoy)', 'Total Debt/Equity (mrq)',
+                                                'Book Value/Share (mrq)', 'Total Cash/Share (mrq)'])
+            snap_financial_table.add_row([yahoo_equity_info['Revenue Per Share (ttm)'], yahoo_equity_info['Quarterly Revenue Growth (yoy)'],
+                                          yahoo_equity_info['Quarterly Earnings Growth (yoy)'], yahoo_equity_info['Total Debt/Equity (mrq)'],
+                                          yahoo_equity_info['Book Value Per Share (mrq)'], yahoo_equity_info['Total Cash Per Share (mrq)']])
+            click.echo(snap_financial_table)
+            #click.echo("Trailing PE: {}\t\tForward PE: {}\tP/S (ttm): {}\t\t\t"
+            #           "P/B (mrq): {}\t\t\tBeta: {}\t\t\tProfit Margin: {}\n"
+            #           "Operating Margin(ttm): {}\tROE: {}\t\tROA: {}\t\t\tRevenue per Share: {}\t"
+            #           "Gross Profit(ttm): {}\tCurrent Ratio (mrq): {}\nQuarterly Revenue Growth (yoy): {}\t\t\t"
+            #           "Quarterly Earnings Growth (yoy): {}\t\t\t\tTotal Debt/Equity (mrq): {}\n"
+            #           "Book Value Per Share (mrq): {}\t\t\tTotal Cash Per Share (mrq): {}\t\t\t\t 1 yr target: {}".
+            #           format(yahoo_equity_info['Trailing P/E'], yahoo_equity_info['Forward P/E 1'],
+            #                  yahoo_equity_info['Price/Sales (ttm)'], yahoo_equity_info['Price/Book (mrq)'],
+            #                  yahoo_equity_info['Beta'], yahoo_equity_info['Profit Margin'],
+            #                  yahoo_equity_info['Operating Margin (ttm)'],
+            #                  yahoo_equity_info['Return on Equity (ttm)'],
+            #                  yahoo_equity_info['Return on Assets (ttm)'],
+            #                  yahoo_equity_info['Revenue Per Share (ttm)'],
+            #                  yahoo_equity_info['Gross Profit (ttm)'],
+            #                  yahoo_equity_info['Current Ratio (mrq)'],
+            #                  yahoo_equity_info['Quarterly Revenue Growth (yoy)'],
+            #                  yahoo_equity_info['Quarterly Earnings Growth (yoy)'],
                               #yahoo_equity_info['Total Cash (mrq)'],
-                              yahoo_equity_info['Total Debt/Equity (mrq)'],
-                              yahoo_equity_info['Book Value Per Share (mrq)'],
-                              yahoo_equity_info['Total Cash Per Share (mrq)'],
-                              yahoo_summary_info['1y Target Est']))
+            #                  yahoo_equity_info['Total Debt/Equity (mrq)'],
+            #                  yahoo_equity_info['Book Value Per Share (mrq)'],
+            #                  yahoo_equity_info['Total Cash Per Share (mrq)'],
+            #                  yahoo_summary_info['1y Target Est']))
 #            click.echo("Market Cap: {}, PE: {},".\
 #                        format(market_cap(rb_client.market_cap),
 #                        click.style(str(rb_client.company_pe_ratio), fg='red')))
-            click.echo("RSI: {}\tHighest: {}\tLowest: {}\t52 week high: {}\t"
-                       "52 week low: {}\tSMA10: {}\tSMA21: {}\tSMA50: {}\t"
-                       "SMA200: {}".format(click.style(str(rsi), fg='red'),
-                                           click.style(str(max(close_price_list)), fg='red'),
-                                           click.style(str(min(close_price_list)), fg='red'),
-                                           click.style(str(rb_client.high_52_weeks), fg='red'),
-                                           click.style(str(rb_client.low_52_weeks), fg='red'),
-                                           sma_10, sma_21, sma_50, sma_200))
+            snap_low_high_table = PrettyTable(['1 yr target', 'RSI', 'Beta', 'Dividend Yield', 'Dividend/Share', 'Highest', 'Lowest', '52 week high', '52 week low', 'SMA10', 'SMA21', 'SMA50', 'SMA200'])
+            snap_low_high_table.add_row([yahoo_summary_info['1y Target Est'], str(rsi), yahoo_equity_info['Beta'], ticker_obj.dividend, ticker_obj.annualDividend,
+                                         str(max(close_price_list)), str(min(close_price_list)), str(rb_client.high_52_weeks),
+                                         str(rb_client.low_52_weeks), sma_10, sma_21, sma_50, sma_200])
+            click.echo(snap_low_high_table)
+            #click.echo("RSI: {}\tHighest: {}\tLowest: {}\t52 week high: {}\t"
+            #           "52 week low: {}\tSMA10: {}\tSMA21: {}\tSMA50: {}\t"
+            #           "SMA200: {}".format(click.style(str(rsi), fg='red'),
+            #                               click.style(str(max(close_price_list)), fg='red'),
+            #                               click.style(str(min(close_price_list)), fg='red'),
+            #                               click.style(str(rb_client.high_52_weeks), fg='red'),
+            #                               click.style(str(rb_client.low_52_weeks), fg='red'),
+            #                               sma_10, sma_21, sma_50, sma_200))
             return True
         fluctuate = get_volatility(float(v['2. high']), float(v['3. low']))
         fluctuate_percent = get_volatility_percent(fluctuate, float(v['4. close']))
