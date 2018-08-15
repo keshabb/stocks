a7521dc8fdc7a85b5a8fc09c511b9cf2443a3b30
Keshab Budhathoky <Keshab.Budhathoky@riverbed.com>
2018-08-15 17:48:32 -0500

    Removing email and password

--- stocks/google.py
+++ stocks/google.py
@@ -6,7 +6,7 @@ import cookielib
 from bs4 import BeautifulSoup
 
 url = "https://www.google.com/finance/portfolio?pid=3&output=csv&action=view&pview=sview&ei=XGBxWcivEoexjAHBw6XYDg";
-values = {'Email': 'kb4it.professional@gmail.com', 'Passwd' : 'Anaconda420', 'signIn' : 'Sign in', 'PersistentCookie' : 'yes'} # The form data 'name' : 'value'
+values = {'Email': '', 'Passwd' : '', 'signIn' : 'Sign in', 'PersistentCookie' : 'yes'} # The form data 'name' : 'value'
 
 cookie = cookielib.CookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
